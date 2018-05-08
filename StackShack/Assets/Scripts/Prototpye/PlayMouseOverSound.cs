feb8c39ef8d3082817e70b22edd1210fd6bd585b
STUDENT\debonte.christoph <debonte.christoph@student.ccm.edu>
2018-05-08 19:05:58 -0400

    Made onHover object to play sound when you hover a button. -CD

---
+++ StackShack/Assets/Scripts/Prototpye/PlayMouseOverSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMouseOverSound : MonoBehaviour {
+
+    public AudioSource playSound;
+    public AudioClip hover;
+
+    public void playOnHover()
+    {
+        playSound.clip = hover;
+        playSound.Play();
+
+    }
+}
